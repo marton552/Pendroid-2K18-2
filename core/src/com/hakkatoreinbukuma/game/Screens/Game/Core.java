b5c7c4bc6c2bda8eda480097e5012dc5989e7ab4
hormartin <pigfighter2@gmail.com>
2018-02-02 10:30:13 +0100

    asd

--- core/src/com/hakkatoreinbukuma/game/Screens/Game/Core.java
+++ core/src/com/hakkatoreinbukuma/game/Screens/Game/Core.java
@@ -1,4 +1,7 @@
 package com.hakkatoreinbukuma.game.Screens.Game;
 
 public class Core {
+    public static float getWindVelocity(float x, float y){
+
+    }
 }
---
+++ core/src/com/hakkatoreinbukuma/game/Screens/Game/WindGenerator.java
@@ -0,0 +1,20 @@
+package com.hakkatoreinbukuma.game.Screens.Game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Matrix3;
+import com.badlogic.gdx.math.Vector2;
+import com.hakkatoreinbukuma.game.MyBaseClasses.Scene2D.OneSpriteStaticActor;
+
+public class WindGenerator extends OneSpriteStaticActor {
+
+    float velocity;
+    Vector2 direction;
+
+    public WindGenerator(Texture texture) {
+        super(texture);
+    }
+
+    public void setPosition(float x, float y) {
+
+    }
+}
