07486ad05a6d7bb27f45139002ba45aa376df3d6
tanulo <pigfighter2@gmail.com>
2018-02-02 10:30:09 +0100

    Ball

---
+++ core/src/com/hakkatoreinbukuma/game/Screens/Game/Ball.java
@@ -0,0 +1,10 @@
+package com.hakkatoreinbukuma.game.Screens.Game;
+
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.hakkatoreinbukuma.game.MyBaseClasses.Scene2D.OneSpriteAnimatedActor;
+
+public class Ball extends OneSpriteAnimatedActor{
+    public Ball(TextureAtlas textureAtlas) {
+        super(textureAtlas);
+    }
+}
